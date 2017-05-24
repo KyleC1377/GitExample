22c97532986a312f1b0ce731339b4df48b9de55e
KyleCheng <Kyle@Kyles-MBP.local>
2017-05-24 14:46:52 +0800

    pdf

--- GitExample/ViewController.swift
+++ GitExample/ViewController.swift
@@ -21,6 +21,12 @@ class ViewController: UIViewController {
         print("Hello World!")
 
     }
+    
+    func testBranch() {
+        print("This is test Branch!!")
+    }
+
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
